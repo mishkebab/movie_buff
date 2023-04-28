099fd6824b5bcd2bc404ff5214f3eb43eb598c06
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-04-28 15:54:39 -0700

    finished defining 01_queries methods

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -71,6 +71,11 @@ def directed_by_one_of(them)
   
   # Note: Directors appear in the 'actors' table.
 
+  Movie
+    .select(:id, :title)
+    .joins('JOIN actors ON actors.id = movies.director_id')
+    .where(actors: {name: them})
+
 end
 
 def movie_names_before_1940
@@ -84,5 +89,7 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+  Movie
+    .where('yr < 1940').pluck(:title)
 
 end
